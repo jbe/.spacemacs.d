2af418a21323235e86330abe8f1e6358d9aceb61
Jostein Eliassen <Jostein Eliassen>
2017-05-21 12:56:16 +0200

    sort dirs first in dired

--- init.el
+++ init.el
@@ -177,6 +177,19 @@
     (cons msg code))
   ;; Specify my function (maybe I should have done a lambda function)
   (setq compilation-exit-message-function 'compilation-exit-autoclose)
+
+  (defun mydired-sort ()
+    "Sort dired listings with directories first."
+    (save-excursion
+      (let (buffer-read-only)
+        (forward-line 2) ;; beyond dir. header
+        (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+      (set-buffer-modified-p nil)))
+
+  (defadvice dired-readin
+      (after dired-after-updating-hook first () activate)
+    "Sort dired listings with directories first before adding marks."
+    (mydired-sort))
 )
 
 (defun dotspacemacs/user-config ()
