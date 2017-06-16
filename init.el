40492a066875b72afbe060442f42a8990b42e90a
Jostein Berre Eliassen <spam@jostein.be>
2017-06-16 15:43:26 +0200

    clean up

--- init.el
+++ init.el
@@ -52,10 +52,7 @@
   ;; This setq-default sexp is an exhaustive list of all the supported
   ;; spacemacs settings.
   (setq-default
-   dotspacemacs-elpa-https t
-   dotspacemacs-elpa-timeout 5
-   dotspacemacs-check-for-update nil
-   dotspacemacs-elpa-subdirectory nil
+   dotspacemacs-check-for-update t
    dotspacemacs-editing-style 'vim ;; `vim', `emacs' or `hybrid'.
    ;; output loading progress in `*Messages*' buffer. (default nil)
    dotspacemacs-verbose-loading nil
@@ -75,7 +72,7 @@
                          spacemacs-light
                          )
 
-   dotspacemacs-colorize-cursor-according-to-state t
+  ;;  dotspacemacs-colorize-cursor-according-to-state t
    dotspacemacs-leader-key "SPC"
    dotspacemacs-emacs-command-key "SPC"
    dotspacemacs-ex-command-key ":"
@@ -83,50 +80,50 @@
    dotspacemacs-major-mode-leader-key ","
    dotspacemacs-major-mode-emacs-leader-key "C-M-m"
    dotspacemacs-distinguish-gui-tab t
-   ;; If non nil `Y' is remapped to `y$' in Evil states. (default nil)
-   dotspacemacs-remap-Y-to-y$ nil
+  ;;  ;; If non nil `Y' is remapped to `y$' in Evil states. (default nil)
+  ;;  dotspacemacs-remap-Y-to-y$ nil
    dotspacemacs-retain-visual-state-on-shift t
    dotspacemacs-visual-line-move-text t
-   ;; If non nil, inverse the meaning of `g' in `:substitute' Evil ex-command.
-   dotspacemacs-ex-substitute-global nil
-   dotspacemacs-default-layout-name "Default"
-   dotspacemacs-display-default-layout nil
-   dotspacemacs-auto-resume-layouts nil
+  ;;  ;; If non nil, inverse the meaning of `g' in `:substitute' Evil ex-command.
+  ;;  dotspacemacs-ex-substitute-global nil
+  ;;  dotspacemacs-default-layout-name "Default"
+  ;;  dotspacemacs-display-default-layout nil
+  ;;  dotspacemacs-auto-resume-layouts nil
    dotspacemacs-large-file-size 1
    dotspacemacs-auto-save-file-location 'cache
-   dotspacemacs-max-rollback-slots 5
-   dotspacemacs-helm-resize nil
-   dotspacemacs-helm-no-header nil
+  ;;  dotspacemacs-max-rollback-slots 5
+  ;;  dotspacemacs-helm-resize nil
+  ;;  dotspacemacs-helm-no-header nil
    dotspacemacs-helm-position 'bottom
    dotspacemacs-helm-use-fuzzy 'always
-   ;; If non nil the paste micro-state is enabled. When enabled pressing `p`
-   ;; several times cycle between the kill ring content. (default nil)
-   dotspacemacs-enable-paste-transient-state nil
-   ;; Which-key delay in seconds. The which-key buffer is the popup listing
-   ;; the commands bound to the current keystroke sequence. (default 0.4)
-   dotspacemacs-which-key-delay 0.4
-   dotspacemacs-which-key-position 'bottom
-   dotspacemacs-loading-progress-bar nil
-   dotspacemacs-fullscreen-at-startup nil
-   dotspacemacs-fullscreen-use-non-native nil
-   dotspacemacs-maximized-at-startup nil
-   ;; Transparency can be toggled through `toggle-transparency'. (default 90)
-   dotspacemacs-active-transparency 90
-   dotspacemacs-inactive-transparency 90
+  ;;  ;; If non nil the paste micro-state is enabled. When enabled pressing `p`
+  ;;  ;; several times cycle between the kill ring content. (default nil)
+  ;;  dotspacemacs-enable-paste-transient-state nil
+  ;;  ;; Which-key delay in seconds. The which-key buffer is the popup listing
+  ;;  ;; the commands bound to the current keystroke sequence. (default 0.4)
+  ;;  dotspacemacs-which-key-delay 0.4
+  ;;  dotspacemacs-which-key-position 'bottom
+  ;;  dotspacemacs-loading-progress-bar nil
+  ;;  dotspacemacs-fullscreen-at-startup nil
+  ;;  dotspacemacs-fullscreen-use-non-native nil
+  ;;  dotspacemacs-maximized-at-startup nil
+  ;;  ;; Transparency can be toggled through `toggle-transparency'. (default 90)
+  ;;  dotspacemacs-active-transparency 90
+  ;;  dotspacemacs-inactive-transparency 90
    dotspacemacs-show-transient-state-title t
    dotspacemacs-show-transient-state-color-guide t
    dotspacemacs-mode-line-unicode-symbols t
    dotspacemacs-smooth-scrolling t
    dotspacemacs-line-numbers t
-   dotspacemacs-folding-method 'origami ;; `evil' or `origami'.
-   dotspacemacs-smartparens-strict-mode nil
-   dotspacemacs-smart-closing-parenthesis nil
-   ;; `any', `current', `all' or `nil'. Default is `all'
-   dotspacemacs-highlight-delimiters 'all
-   dotspacemacs-persistent-server nil
+  ;;  ;; dotspacemacs-folding-method 'origami ;; `evil' or `origami'.
+  ;;  dotspacemacs-smartparens-strict-mode nil
+  ;;  dotspacemacs-smart-closing-parenthesis nil
+  ;;  ;; `any', `current', `all' or `nil'. Default is `all'
+  ;;  dotspacemacs-highlight-delimiters 'all
+  ;;  dotspacemacs-persistent-server nil
    dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
-   ;; Not used for now. (default nil)
-   dotspacemacs-default-package-repository nil
+  ;;  ;; Not used for now. (default nil)
+  ;;  dotspacemacs-default-package-repository nil
    dotspacemacs-whitespace-cleanup 'trailing
 
    js2-basic-offset 2
@@ -192,17 +189,16 @@
       (after dired-after-updating-hook first () activate)
     "Sort dired listings with directories first before adding marks."
     (mydired-sort))
-
-  (hlinum-activate)
 )
 
 (defun dotspacemacs/user-config ()
   ;; (global-hl-line-mode -1) ; Disable current line highlight
   (global-linum-mode) ; Show line numbers by default
   (global-company-mode)
+  (hlinum-activate)
 
-  (setq split-height-threshold nil)
-  (setq split-width-threshold 200)
+  ;; (setq split-height-threshold nil)
+  ;; (setq split-width-threshold 200)
 
   (define-key evil-insert-state-map (kbd "C-c") 'evil-escape)
   (define-key evil-visual-state-map (kbd "C-c") 'evil-escape)
@@ -236,17 +232,4 @@
 
   (doom-themes-neotree-config)
 )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (origami xterm-color ws-butler winum which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package unfill toc-org tagedit stickyfunc-enhance srefactor spaceline smeargle slim-mode shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe restart-emacs rbenv rake rainbow-delimiters pug-mode powershell popwin persp-mode paradox orgit org-projectile org-present org-pomodoro org-plus-contrib org-download org-bullets open-junk-file neotree mwim multi-term move-text mmm-mode minitest markdown-toc magit-gitflow macrostep lorem-ipsum livid-mode linum-relative link-hint less-css-mode json-mode js2-refactor js-doc info+ indent-guide hungry-delete htmlize hlinum hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot glsl-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fuzzy flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav dumb-jump doom-themes disaster diff-hl define-word company-web company-tern company-statistics company-quickhelp company-c-headers column-enforce-mode coffee-mode cmake-mode clean-aindent-mode clang-format chruby bundler auto-yasnippet auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
